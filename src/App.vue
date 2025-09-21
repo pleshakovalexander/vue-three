934fe4e76e0ca8cdde1bee3c41efc21e7e8089cc
Alexander Pleshakov <sh.plh@yandex.ru>
2025-09-21 17:57:23 +0300

    fix resize

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,8 @@ let animationId: number
 
 onMounted(() => {
   scene = new THREE.Scene()
+  scene.background = new THREE.Color(0x87ceeb) // sky blue
+
   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
   renderer = new THREE.WebGLRenderer({ antialias: true })
@@ -61,9 +63,7 @@ onBeforeUnmount(() => {
 })
 
 function onWindowResize() {
-  camera.aspect =
-    container.value?.clientWidth / container.value?.clientHeight ||
-    window.innerWidth / window.innerHeight
+  camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
   renderer.setSize(window.innerWidth, window.innerHeight)
 }
